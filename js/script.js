cbdc1281131706ed62ba244754d0c41f8847a3de
Sam <ampersamd@gmail.com>
2017-01-09 17:48:18 -0500

    Redesign
    -Changed background to gradient from changing gifs
    -Redesigned how weather content displays
    -Implement new input search bar (not functional yet)

--- js/script.js
+++ js/script.js
@@ -12,32 +12,53 @@ var weatherGif = {"Rain": "https://media.giphy.com/media/3osxYcKIttf3N5GTsY/giph
                   "Clear": "https://media.giphy.com/media/7BurliewSdqFO/giphy.gif",
                   "Fog": "https://media.giphy.com/media/l0MYRbawhDAnyMG4w/source.gif"};
 
-$(document).ready(function () {
+function search(e) {
     "use strict";
-    $("#submit").click(function () {
-        var zipCode = document.getElementById("zip").value;
-
-        $.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + ",us&APPID=30851acc20d637dee2432358d1f40e1c", function (json) {
-            $(".location").html(json.name + ", " + json.sys.country);
-            $(".conditions").html(json.weather[0].description);
-            var tempKRaw = json.main.temp,
-                tempKDisplay = Math.round(json.main.temp) + " °K",
-                tempFRaw = 1.8 * (tempKRaw - 273) + 32,
-                tempFDisplay = Math.round(tempFRaw) + " °F",
-                tempCRaw = tempKRaw - 273,
-                tempCDisplay = Math.round(tempCRaw) + " °C";
+    e.preventDefault();
+    
+    if ($('#fah').length < 1) {
+        $("#buttonBar").append('<button type="button" id="fah" class="btn">°F</button>');
+        $("#buttonBar").append('<button type="button" id="cel" class="btn">°C</button>');
+    }
+    
+    var zipCode = document.getElementById("zip").value;
+    $("#zipRequest").html("");
+    //gets weather JSON data from OpenWeather API
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + ",us&APPID=30851acc20d637dee2432358d1f40e1c", function (json) {
+        $(".location").html(json.name + ", " + json.sys.country);
+        $(".conditions").html(json.weather[0].description);
+        var tempKRaw = json.main.temp,
+            tempKDisplay = Math.round(json.main.temp) + " °K",
+            tempFRaw = 1.8 * (tempKRaw - 273) + 32,
+            tempFDisplay = Math.round(tempFRaw) + " °F",
+            tempCRaw = tempKRaw - 273,
+            tempCDisplay = Math.round(tempCRaw) + " °C";
+        $(".temp").html(tempFDisplay);
+            
+            
+        //Previous code to change background to gif
+        //$(document.body).css("background", "url('" + weatherGif[json.weather[0].main] + "') no-repeat center center fixed");
+        //$(document.body).css("background-size", "cover");
+            
+      
+        $("#cel").click(function () {
+            $(".temp").html(tempCDisplay);
+        });
+      
+        $("#fah").click(function () {
             $(".temp").html(tempFDisplay);
-     
-            $(document.body).css("background", "url('" + weatherGif[json.weather[0].main] + "') no-repeat center center fixed");
-            $(document.body).css("background-size", "cover");
-      
-            $("#cel").click(function () {
-                $(".temp").html(tempCDisplay);
-            });
-      
-            $("#fah").click(function () {
-                $(".temp").html(tempFDisplay);
-            });
         });
     });
+}
+
+$(document).ready(function () {
+    "use strict";
+//    $("#submit").one('click', function () {
+//        //add conversion buttons to HTML flow
+//        $("#buttonBar").append('<button type="button" id="fah" class="btn">°F</button>');
+//        $("#buttonBar").append('<button type="button" id="cel" class="btn">°C</button>');
+//    });
+    
+    $("#submit").click(search);
+    $(".form-inline").submit(search);
 });
